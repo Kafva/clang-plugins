72121245c26c00c59f3b6705986e2c1ef8143850
Kafva <Martensson.Jonas98@gmail.com>
2022-05-01 18:09:27 +0200

    EUF update

--- run.py
+++ run.py
@@ -26,7 +26,7 @@ SOURCE_SUB_DIR=f"{TARGET_DIR}/lib"
 
 if __name__ == '__main__':
     CONFIG.update_from_file(f"{BASE_DIR}/../examples/expat/base.json")
-    subdir_tus = get_subdir_tus(TARGET_DIR, TARGET_DIR)
+    subdir_tus = get_subdir_tus(TARGET_DIR)
     subdir_tu  = subdir_tus[SOURCE_SUB_DIR]
     outdir = f"{BASE_DIR}/.states"
     mkdir_p(outdir)
