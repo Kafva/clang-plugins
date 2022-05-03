d4877ead6fc86af3d94479c59dff37409242e928
Kafva <Martensson.Jonas98@gmail.com>
2022-05-03 09:52:20 +0200

    Update run.py

--- run.py
+++ run.py
@@ -2,9 +2,9 @@
 '''
 This script assumes that clang-plugins is being ran as a submodule in euf
 '''
+import sys
 from pathlib import Path
 from posixpath import expanduser
-import sys
 
 sys.path.append("..")
 
@@ -19,23 +19,25 @@ SYMBOL_LIST=f"{BASE_DIR}/../tests/expected/libexpat_90ed_ef31_change_set.txt"
 
 #TARGET_DIR=f"{expanduser('~')}/Repos/jabberd-2.7.0"
 #SOURCE_SUB_DIR=f"{TARGET_DIR}/sx"
-
 TARGET_DIR=f"{expanduser('~')}/.cache/euf/libexpat-90ed5777/expat"
-#SOURCE_SUB_DIR=f"{TARGET_DIR}/xmlwf"
-SOURCE_SUB_DIR=f"{TARGET_DIR}/lib"
+SOURCE_SUB_DIR=f"{TARGET_DIR}/xmlwf"
 
 if __name__ == '__main__':
     CONFIG.update_from_file(f"{BASE_DIR}/../examples/base_expat.json")
+    CONFIG.CLANG_PLUGIN_RUN_STR_LIMIT = 10000
     subdir_tus = get_subdir_tus(TARGET_DIR)
-    subdir_tu  = subdir_tus[SOURCE_SUB_DIR]
     outdir = f"{BASE_DIR}/.states"
     mkdir_p(outdir)
     remove_files_in(outdir)
 
     with open(SYMBOL_LIST, mode = 'r', encoding='utf8') as f:
-        for sym in f.readlines():
-            sym = sym.rstrip('\n')
-            print(f"===> {sym} <===")
-            call_arg_states_plugin(sym, outdir, TARGET_DIR, SOURCE_SUB_DIR,
-                    subdir_tu, quiet=QUIET, setx=True)
-            break
+        for subdir in subdir_tus.keys():
+            if subdir != SOURCE_SUB_DIR: continue
+            print(f"===> {subdir} <===")
+            subdir_tu = subdir_tus[subdir]
+            for sym in f.readlines():
+                sym = sym.rstrip('\n')
+                print(f"===> {sym} <===")
+                call_arg_states_plugin(sym, outdir, subdir,
+                        subdir_tu, quiet=QUIET, setx=True)
+                break
